0ebca2cf2a3d15b226c855002b6f8a5a3c2eb3f8
김민수 <gimminsu@192.168.nate.com>
2021-07-19 15:56:20 +0900

    baseURL 변경

--- sagas/index.ts
+++ sagas/index.ts
@@ -2,8 +2,7 @@ import { all, fork } from "@redux-saga/core/effects";
 import userSaga from "./user";
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "https://ec2-52-79-98-228.ap-northeast-2.compute.amazonaws.com";
+axios.defaults.baseURL = "https://petpairs.de";
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
